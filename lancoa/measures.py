b157187579ec91c0d1ff74997abcf53093affe3c
domargan <margan.domagoj@gmail.com>
2015-02-19 00:00:24 +0100

    Added function for calculation of selectivity entropy.

--- lancoa/measures.py
+++ lancoa/measures.py
@@ -184,3 +184,30 @@ def entropy_out_selectivity(network):
     entropy = -(entropy)/math.log(n)
 
     return entropy
+
+
+def entropy_selectivity(network):
+    g = nx.read_weighted_edgelist(network)
+    n = g.number_of_nodes()
+
+    entropy = 0
+    sel_sum = 0
+    selctivity_sequence = []
+
+    for node in g.nodes():
+        s = g.degree(node, weight='weight')
+        k = g.degree(node, weight=None)
+        if k > 0:
+            selectivity = s / k
+        else:
+            selectivity = 0
+        sel_sum += selectivity
+        selctivity_sequence.append(selectivity)
+
+    for selectivity in selctivity_sequence:
+        if selectivity > 0:
+            entropy += ((selectivity / float(sel_sum)) * (math.log(selectivity / float(sel_sum))))
+
+    entropy = -(entropy)/math.log(n)
+
+    return entropy
