34156d13645765e0123b11dd411a7f44a81a13cf
domargan <margan.domagoj@gmail.com>
2015-02-18 22:07:30 +0100

    Added module for caluculation of network measures.

---
+++ lancoa/measures.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2015 Domagoj Margan <margan.domagoj@gmail.com>
+
+This file is part of LaNCoA.
+LaNCoA is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+LaNCoA is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with LaNCoA.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import networkx as nx
+
+
+def reciprocity(network):
+    g = nx.read_weighted_edgelist(network, create_using=nx.DiGraph())
+
+    self_loops = g.number_of_selfloops()
+    r = sum([g.has_edge(e[1], e[0])
+                    for e in g.edges_iter()]) / float(g.number_of_edges())
+
+    a = (g.number_of_edges() - self_loops) / (float(g.number_of_nodes()) * float((g.number_of_nodes() - 1)))
+
+    ro = float((r - a)) / float((1 - a))
+
+    return r, a, ro
